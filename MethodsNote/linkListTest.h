ef9130bb3d4a9f4c06e5812a4af81c12f04ad0f8
lizhihao <2048220108@qq.com>
2022-08-13 11:29:58 +0800

    two

--- MethodsNote/linkListTest.h
+++ MethodsNote/linkListTest.h
@@ -3,10 +3,6 @@
 #include"linkList.h"
 #include "oftenUseBaseData.h"
 void initLinkListTest() {
-	LinkList list, LB, C;
-	LNode *p, * q;
-	initLinkList(list);
-	initLinkList(LB);
-	initLinkList(C);
+	
 	
 }   
